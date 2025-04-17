48099f124444e69bd9ce53e540275129ad9f76e0
Dipesh12-pokhrel <bwit24d.dps@ismt.edu.np>
2025-04-17 07:45:56 +0545

    Add files via upload

---
+++ dbo.Booking.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Booking] (
+    [Id]        INT           NOT NULL,
+    [firstName] NVARCHAR (50) NOT NULL,
+    [surname]   NVARCHAR (50) NOT NULL,
+    [address] NVARCHAR(50) NOT NULL, 
+    [age] INT NOT NULL, 
+    [license] BIT NOT NULL, 
+    [numberOfDaYS] INT NOT NULL, 
+    [carType] NVARCHAR(50) NOT NULL, 
+    [fuelType] NVARCHAR(50) NOT NULL, 
+    [unLimitedMileage] BIT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ dbo.Table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [firstName] NVARCHAR(50) NOT NULL, 
+    [surname] NVARCHAR(50) NOT NULL, 
+    [address] NVARCHAR(50) NOT NULL, 
+    [carType] NVARCHAR(50) NOT NULL, 
+    [fuelType] NVARCHAR(50) NOT NULL, 
+    [days] INT NOT NULL, 
+    [unMileage] BIT NULL, 
+    [breakDownCover] BIT NULL, 
+    [total] DECIMAL NOT NULL
+)
